b2292578588af26a22b34e12fd04067b9b886700
Mel4sa <melisaxsimsek@gmail.com>
2025-07-16 08:06:40 +0300

    create model

---
+++ FoodManager/FoodManager/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  FoodManager
+//
+//  Created by Melisa Şimşek on 16.07.2025.
+//
+
+
+import Foundation
+
+struct Meal: Codable {
+    let name: String
+    let calories: String
+}
--- FoodManager/FoodManager/ViewController.swift
+++ FoodManager/FoodManager/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var caloriesTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func addMealTapped(_ sender: UIButton) {
+        // eklenecek
     }
 
 
