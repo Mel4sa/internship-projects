1153acc8e201cbf4a6faf07ad99cfee5c889f3a0
Mel4sa <melisaxsimsek@gmail.com>
2025-07-31 14:05:35 +0300

    finished

---
+++ Game/Game/BoardCell.swift
@@ -0,0 +1,15 @@
+//
+//  BoardCell.swift
+//  Game
+//
+//  Created by Melisa Şimşek on 31.07.2025.
+//
+
+
+import UIKit
+
+class BoardCell: UICollectionViewCell
+{
+    @IBOutlet weak var image: UIImageView!
+}
+
